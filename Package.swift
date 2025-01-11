14bfb1eed0094af9a980df273b06806b87aeec2e
Telem Tobi <telem16@gmail.com>
2025-01-11 08:17:20 +0200

    Set up Package.swift

--- Package.swift
+++ Package.swift
@@ -2,19 +2,35 @@
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
+import CompilerPluginSupport
 
 let package = Package(
     name: "swift-localization",
+    platforms: [.iOS(.v13), .macOS(.v10_15), .tvOS(.v13), .watchOS(.v6),],
     products: [
         .library(name: "Localization", targets: ["Localization"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-syntax.git", "509.0.0"..<"601.0.0")
+    ],
     targets: [
+        .macro(
+            name: "LocalizationMacros",
+            dependencies: [
+                .product(name: "SwiftSyntaxMacros", package: "swift-syntax"),
+                .product(name: "SwiftCompilerPlugin", package: "swift-syntax")
+            ]
+        ),
         .target(
-            name: "Localization"
+            name: "Localization",
+            dependencies: ["LocalizationMacros"]
         ),
         .testTarget(
             name: "LocalizationTests",
-            dependencies: ["Localization"]
-        ),
+            dependencies: [
+                "LocalizationMacros",
+                .product(name: "SwiftSyntaxMacrosTestSupport", package: "swift-syntax")
+            ]
+        )
     ]
 )
