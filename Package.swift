204353d8964c311db68c05e9f6a9add111c5a233
Telem Tobi <telem16@gmail.com>
2025-01-11 07:56:35 +0200

    Initial naming

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 5.10
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,19 +6,15 @@ import PackageDescription
 let package = Package(
     name: "swift-localization",
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "swift-localization",
-            targets: ["swift-localization"]),
+        .library(name: "Localization", targets: ["Localization"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "swift-localization"),
+            name: "Localization"
+        ),
         .testTarget(
-            name: "swift-localizationTests",
-            dependencies: ["swift-localization"]
+            name: "LocalizationTests",
+            dependencies: ["Localization"]
         ),
     ]
 )
